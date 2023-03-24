936a5719e5148367bd80cf5da9785c1505fccaf9
25thMazino <ahijahabba01@gmail.com>
2023-03-24 15:57:21 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies integers
+ *
+ * @a: parameter function
+ * @b: parameter function
+ *
+ * Return: x, y
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
