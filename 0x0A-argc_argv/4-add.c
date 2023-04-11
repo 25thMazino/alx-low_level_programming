2019a6342562160a785b2e9ef1f0246dd1d2cf5c
25thMazino <ahijahabba01@gmail.com>
2023-04-10 19:39:39 -0700

    to infinity

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sting.h>
+#include <string.h>
 
 /**
  * main - adds positive numbers
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 {
 	int i = 0;
 	unsigned int j, sum  = 0;
-	char *k
+	char *k;
 
 	if (argc > 1)
 	{
